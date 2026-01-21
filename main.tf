5d86183ada2aed0915631b5f62797ec0fd6a897d
mohdaaleem2111-creator <mohdaaleem2111@gmail.com>
2026-01-21 15:31:58 +0530

    Change main.ts file

--- main.tf
+++ main.tf
@@ -1,4 +1,12 @@
-resource "local_file" "my_pets" {
-  filename = "pets.txt"
-  content = "I love pets"
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "demo" {
+  ami           = "ami-0f58b397bc5c1f2e8"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Jenkins-Terraform"
+  }
 }
